b5abf6ff7cf748f150cc69c5b43b754f7590a498
Arthur Correia <arthurcorreia260y@gmail.com>
2026-03-20 23:26:13 -0300

    Primeiro commit do projeto DevFLix

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+function Footer(){
+        return(
+            <footer className={styles.footer}>
+                <h2>DevFlix &copy; Desevolvido por Arthur Correia 2026</h2>
+            </footer>
+        );
+}
+
+export default Footer;
