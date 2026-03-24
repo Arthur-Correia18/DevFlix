150a1d281ad2c9c1f7f1e7f1d6253c094d374735
Arthur Correia <arthurcorreia260y@gmail.com>
2026-03-23 21:25:32 -0300

    Novo arquvo (vdeos.json), atualize o import de mapeamento dos videos criei o componente category, estilizei o CSS do componente Category,filtrei os videos por categorias, criei a functon filterCategory!

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import Banner from "./components/Banner";
 import Card from "./components/Card";
+import Category from "./components/Category";
 import Container from "./components/Container";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import videos from "./json/db.json";
+import videos from "./json/videos.json";
 
+const categories = [
+  "Comedia",
+  "Romance",
+  "Terror",
+  "Suspense",
+  "Drama",
+]
 
+function filterCategory(id) {
+  return videos.filter(video => video.category === categories[id] )
+}
 
 function App() {
   return (
@@ -13,10 +24,30 @@ function App() {
       <Header />
       <Banner image="home"/>
       <Container>
-        <h2>Comedia</h2>
-        <section className="cards">
-          { videos.map((video) => <Card id={video.id} key={video.id} />)}
-        </section>
+
+        <Category category="Comedia">
+           { filterCategory(0).map((video) => <Card id={video.id} key={video.id} />)}
+        </Category>
+
+         <Category category="Romance">
+           { filterCategory(1).map((video) => <Card id={video.id} key={video.id} />)}
+        </Category>
+
+         <Category category="Terror">
+           { filterCategory(2).map((video) => <Card id={video.id} key={video.id} />)}
+        </Category>
+
+         <Category category="Suspense">
+           { filterCategory(3).map((video) => <Card id={video.id} key={video.id} />)}
+        </Category>
+
+         <Category category="Drama">
+           { filterCategory(4).map((video) => <Card id={video.id} key={video.id} />)}
+        </Category>
+
+
+
+
      </Container>
      <Footer />
     </>
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,10 +3,10 @@ function Card({ id }) {
     return (
         <section className={styles.card}>
             <a 
-            href={`https://www.youtube.com/watch?v=${id}`}
-            rel="noreferrer noopener"
-            target="_blank">
-            <img src={`https://img.youtube.com/vi/${id}/mqdefault.jpg`} alt="Capa" />
+                href={`https://www.youtube.com/watch?v=${id}`}
+                rel="noreferrer noopener"
+                target="_blank">
+                <img src={`https://img.youtube.com/vi/${id}/mqdefault.jpg`} alt="Capa" />
             </a>    
         </section>
     );
