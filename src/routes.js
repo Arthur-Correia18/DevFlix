f76d64ab790e9dc1e1afb14e0f8d73979bc7c2bb
Arthur Correia <arthurcorreia260y@gmail.com>
2026-03-26 17:32:57 -0300

    feat(DevFlix): adiciona homepage com estrutura de componentes e organização inicial do layout

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Watch from "./pages/Watch";
+
+function AppRoutes() {
+    return (
+        <BrowserRouter basename="/DevFlix">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/watch" element={<Watch />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default AppRoutes;  
